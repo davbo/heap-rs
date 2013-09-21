40f004f489d843172857cf80d56c90d3f7a0b5cf
Dave King <dave@davbo.org>
2013-09-21 10:59:35 +0100

    Updated with an in-place solution

--- heap.rs
+++ heap.rs
@@ -1,50 +1,49 @@
 trait Heap {
-    fn build_max_heap(&mut self);
-    fn add_element(&mut self, int);
-    fn max_heapify(&mut self, index: uint);
-    fn heapsort(&mut self) -> ~[int];
+    fn build_max_heap(&mut self, offset: uint);
+    fn max_heapify(&mut self, index: uint, offset: uint);
+    fn heapsort(&mut self);
 }
 
 impl Heap for ~[int] {
-    fn build_max_heap(&mut self) {
-        let mut index = (self.len() / 2) + 1;
+    fn build_max_heap(&mut self, offset: uint) {
+        let length = self.len() - offset;
+        let mut index = (length / 2) + 1;
         while index > 0 {
             index = index - 1;
-            self.max_heapify(index);
+            self.max_heapify(index, offset);
         }
     }
-    fn add_element(&mut self, elem: int) {
-        self.push(elem);
-    }
-    fn max_heapify(&mut self, index: uint) {
+    fn max_heapify(&mut self, index: uint, offset: uint) {
+        let length = self.len() - offset;
         let left = (index * 2) + 1;
         let right = (index * 2) + 2;
         let mut largest = index;
-        if (left < self.len() && (self[left] > self[largest])) {
+        if (left < length && (self[left] > self[largest])) {
             largest = left;
         }
-        if (right < self.len() && (self[right] > self[largest])) {
+        if (right < length && (self[right] > self[largest])) {
             largest = right;
         }
         if (largest != index) {
             self.swap(index, largest);
-            self.max_heapify(largest);
         }
     }
-    fn heapsort(&mut self) -> ~[int] {
-        self.build_max_heap();
-        let mut result = ~[];
-        while self.len() > 0 {
-            result.push(self.remove(0));
-            self.build_max_heap();
+    fn heapsort(&mut self) {
+        let mut offset = 0;
+        let length = self.len();
+        self.build_max_heap(offset);
+        while offset != length {
+            offset = offset + 1;
+            let top = self.remove(0);
+            self.push(top);
+            self.build_max_heap(offset);
         }
-        result
     }
 }
 
 fn main() {
-    let mut a = ~[5,2,6,3,7];
+    let mut a = ~[5,2,532, 12412, 1243215, 3262, 12 ,6,3,7];
     println(a.to_str());
-    let b = a.heapsort();
-    println(b.to_str());
+    a.heapsort();
+    println(a.to_str());
 }
